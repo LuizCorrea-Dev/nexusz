644592f5e030a1ed37ceea90b77241bc082a88b9
Luiz Correa <correa.luizbatista@gmail.com>
2025-06-07 23:27:06 +0100

    correcao de  nomes de styles case-sensitive

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
-import "../styles/global.css"; // <-- global.css aqui
-import "../styles/Home.module.css"; // CSS Modules continuam normais
-import "../styles/Card.module.css"; // Import específico continua
+import "../styles/Global.css";
+import "../styles/Home.module.css";
+import "../styles/Card.module.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
