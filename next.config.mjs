9ec1a7524abb2634ab4a93f17cbaf3bd076bd832
Luiz Correa <63646335+LuizCorrea-Dev@users.noreply.github.com>
2025-06-08 21:15:32 +0100

    feat: adicionar configuração experimental para o uso do pages router no next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: false, // você usa pages router (não app router)
+  },
+  pageExtensions: ["js", "jsx"],
   reactStrictMode: true,
   output: "standalone", // ✅ Modo Server Side Rendering
 };
