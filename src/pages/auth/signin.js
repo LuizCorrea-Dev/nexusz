d2e92561d5f8e3f732d46055825c456b64e76a62
Luiz Correa <63646335+LuizCorrea-Dev@users.noreply.github.com>
2025-06-08 23:41:55 +0100

    feat: adicionar log de autenticação e melhorar a estrutura do componente SignIn

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -1,19 +1,53 @@
 "use client";
-import { signIn } from "next-auth/react";
+
+import { useSession, signIn, signOut } from "next-auth/react";
 import styles from "../../styles/signin.module.css";
+import { useEffect } from "react";
 
 export default function SignIn() {
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      // Log SteamID
+      console.log(
+        "✅ Logado com SteamID:",
+        session.user?.id || "ID não disponível"
+      );
+    } else if (status === "unauthenticated") {
+      console.error("❌ Usuário não está logado!");
+    }
+  }, [session, status]);
+
   return (
     <div className={styles.container}>
       <div className={styles.loginBox}>
         <h1 className={styles.title}>🧬 Nexus Z - Login</h1>
         <p className={styles.subtitle}>Sobreviva. Descubra. Conquiste.</p>
-        <button
-          onClick={() => signIn("steam", { callbackUrl: "/" })}
-          className={styles.steamButton}
-        >
-          Login com Steam
-        </button>
+
+        {session ? (
+          <>
+            <img
+              src={session.user?.image}
+              alt="Avatar Steam"
+              className={styles.avatar}
+            />
+            <p className={styles.username}>{session.user?.name}</p>
+            <button
+              className={`${styles.steamButton} ${styles.logged}`}
+              onClick={() => signOut()}
+            >
+              Deslogar
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={() => signIn("steam", { callbackUrl: "/" })}
+            className={styles.steamButton}
+          >
+            Login com Steam
+          </button>
+        )}
       </div>
     </div>
   );
