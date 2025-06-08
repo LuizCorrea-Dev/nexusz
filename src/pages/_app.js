feca9730fafa12d67b4d0054714c1e03154dc250
Luiz Correa <63646335+LuizCorrea-Dev@users.noreply.github.com>
2025-06-08 16:24:20 +0100

    Feat: Adicionar componente Loader com animação de carregamento e ajustes de estilo na página

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import "../styles/global.css";
 import "../styles/home.module.css";
 import "../styles/card.module.css";
+import Loader from "../components/Loader";
 
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula carregamento, ou espere algum evento real
+    setTimeout(() => setLoading(false), 12000); // 12 segundos
+  }, []);
+
+  if (loading) {
+    return <Loader onFinish={() => setLoading(false)} />;
+  }
+
   return <Component {...pageProps} />;
 }
