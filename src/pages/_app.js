7040966a0788e2a71712b8cf46deebe9414c6ffb
Luiz Correa <63646335+LuizCorrea-Dev@users.noreply.github.com>
2025-06-08 18:50:43 +0100

    Feat: Remover lógica de carregamento e componente Loader do App para simplificar a estrutura e melhorar a performance

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,32 +1,7 @@
-import { useState, useEffect } from "react";
 import "../styles/global.css";
 import "../styles/home.module.css";
 import "../styles/card.module.css";
-import Loader from "../components/Loader";
 
 export default function App({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (!sessionStorage.getItem("loadedOnce")) {
-        setLoading(true);
-
-        const timer = setTimeout(() => {
-          setLoading(false);
-          sessionStorage.setItem("loadedOnce", "true"); // Marca que já carregou uma vez
-        }, 5500); // Tempo do loader + delay
-
-        return () => clearTimeout(timer);
-      } else {
-        setLoading(false);
-      }
-    }
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
-
   return <Component {...pageProps} />;
 }
