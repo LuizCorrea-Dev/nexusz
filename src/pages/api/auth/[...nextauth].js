53c3fef5b31530744918bbaa1ef1f23df2c4166f
Luiz Correa <63646335+LuizCorrea-Dev@users.noreply.github.com>
2025-06-10 19:02:02 +0100

    fix: atualizar dependências do NextAuth e remover o provedor Steam

--- src/pages/api/auth/[...nextauth].js
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptionsWithRequest } from "@/lib/auth";
-
-export default async (req, res) => {
-  return await NextAuth(req, res, authOptionsWithRequest(req));
-};
